c4f11095fa55ba74032de97f89ac32825607d1c0
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-07 06:35:16 +0900

    main color changed

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,29 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         key: scaffoldKey,
         appBar: AppBar(
-            leading: Icon(Icons.menu),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
             title: const Text('Metropolitan Wallpaper'),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                },
-              )
-            ],
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple, Colors.deepOrangeAccent],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft
+              ),
+            ),
+          ),
         ),
       ),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
     );
