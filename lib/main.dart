1a654a50be23c0b115b43911786e90143eec7c7a
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-07 10:26:24 +0900

    add drawer func

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,12 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         key: scaffoldKey,
         appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-            ),
             title: const Text('Metropolitan Wallpaper'),
           actions: [
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {},
+              onPressed: () {
+              },
             )
           ],
           flexibleSpace: Container(
@@ -34,6 +31,41 @@ class MyApp extends StatelessWidget {
                 end: Alignment.topLeft
               ),
             ),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                    color: Colors.deepOrangeAccent
+                ),
+                child: Text(
+                  'br',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.search),
+                title: Text('Search'),
+              ),
+              ListTile(
+                leading: Icon(Icons.category),
+                title: Text('Category'),
+              ),
+              ListTile(
+                leading: Icon(Icons.list),
+                title: Text('My Lists'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
           ),
         ),
       ),
