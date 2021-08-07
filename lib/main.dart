c9027147481f2ad6968d7c6fef6f2a0a49e1a22c
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-07 10:47:18 +0900

    Refacor for drawer parts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaper/navigation.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,72 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-            title: const Text('Metropolitan Wallpaper'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-              },
-            )
-          ],
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.purple, Colors.deepOrangeAccent],
-                begin: Alignment.bottomRight,
-                end: Alignment.topLeft
-              ),
-            ),
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                    color: Colors.deepOrangeAccent
-                ),
-                child: Text(
-                  'br',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.search),
-                title: Text('Search'),
-              ),
-              ListTile(
-                leading: Icon(Icons.category),
-                title: Text('Category'),
-              ),
-              ListTile(
-                leading: Icon(Icons.list),
-                title: Text('My Lists'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ],
-          ),
-        ),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      home: Navigation(),
     );
   }
 }
---
+++ lib/navigation.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  @override
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Metropolitan Wallpaper'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+            },
+          )
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.purple, Colors.deepOrangeAccent],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  color: Colors.deepOrangeAccent
+              ),
+              child: Text(
+                'br',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.search),
+              title: Text('Search'),
+            ),
+            ListTile(
+              leading: Icon(Icons.category),
+              title: Text('Category'),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('My Lists'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
